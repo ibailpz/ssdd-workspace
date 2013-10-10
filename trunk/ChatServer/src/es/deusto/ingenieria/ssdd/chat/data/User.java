1453cfbb95152f78d5dd4f16235a01f8e818d6b3
hasier.rodriguez@opendeusto.es <hasier.rodriguez@opendeusto.es@4ccc3f26-942a-7184-b107-c4d0cda8679f>
2013-10-10 13:59:44 +0000

    Minor User fix, add ip and port

--- trunk/ChatServer/src/es/deusto/ingenieria/ssdd/chat/data/User.java
+++ trunk/ChatServer/src/es/deusto/ingenieria/ssdd/chat/data/User.java
@@ -1,20 +1,27 @@
 package es.deusto.ingenieria.ssdd.chat.data;
 
-public class User {	
+public class User {
+	private String ip;
+	private int port;
 	private String nick;
 	private int state = 1;
-	
+
+	public User(String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+
 	public String getNick() {
 		return nick;
 	}
-	
+
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
-		
+
 	public boolean equals(Object obj) {
-		if (obj != null && obj.getClass().equals(this.getClass())) {			
-			return this.nick.equalsIgnoreCase(((User)obj).nick);				  
+		if (obj != null && obj.getClass().equals(this.getClass())) {
+			return this.nick.equalsIgnoreCase(((User) obj).nick);
 		} else {
 			return false;
 		}
@@ -26,5 +33,13 @@ public class User {
 
 	public void setState(int state) {
 		this.state = state;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public int getPort() {
+		return port;
 	}
 }
